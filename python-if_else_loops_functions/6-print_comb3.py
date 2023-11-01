0b40e0110fa9800b933da1fe624738831a405a39
l-isaro <l.isaro@alustudent.com>
2023-11-01 09:37:49 -0700

    praying

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for n in range(10):
-        if i < n:
-            print("{}{}, ".format(i, n), sep='', end='')
-print()
+        if i == 8 and n == 9:
+            print("{}{}".format(i, n))
+        elif i < n:
+            print("{}{}, ".format(i, n), end='')
