de0ba9e034586c80ba89c3e5e1623af6adc1bb46
l-isaro <l.isaro@alustudent.com>
2023-11-01 07:44:50 -0700

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(101):
-    if i == 100:
-        print()
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
     else:
-        print("{:02d}, ".format(i), sep=", ",end='')
+        print("{:02d}, ".format(i), end='')
         
