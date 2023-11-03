9e6dd17044a39fffbcfc4473856157f80e40cb0b
l-isaro <l.isaro@alustudent.com>
2023-11-03 02:21:37 -0700

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for i in range(len(sys.argv)):
+        if i > 0:
+            sum = sum + int(sys.argv[i])
+    print(sum)
+        
