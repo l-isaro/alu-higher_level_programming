719f3b38d9cfb877377f2e05af5de1f7da65a856
l-isaro <l.isaro@alustudent.com>
2023-11-03 02:08:53 -0700

    I think I got it right

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) > 1:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
             if i > 0:
                 print("{}: {}".format(i, sys.argv[i]))
