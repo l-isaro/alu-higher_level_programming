2e81b01187e75657b0ccf2783d60c67685d88314
l-isaro <l.isaro@alustudent.com>
2023-11-03 01:50:24 -0700

    blah

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-import sys
-if len(sys.argv) > 1:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
-        if i > 0:
-            print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("0 arguments.")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("0 arguments.")
