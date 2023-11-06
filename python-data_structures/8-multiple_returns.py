268629c3681c71658fcdb2cc0fa5eef403b54957
l-isaro <l.isaro@alustudent.com>
2023-11-05 23:37:34 -0800

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     if len(sentence) > 0:
         f_letter = sentence[0]
         length = len(sentence)
-        return length, f_letter
     else:
         f_letter = None
-        return f_letter
+        length = 0
+    return length, f_letter
