d1329944d15c3cef13d8e866491645ec909edce0
l-isaro <l.isaro@alustudent.com>
2023-11-08 10:07:33 -0800

    6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     keys = sorted(a_dictionary)
     for i in keys:
-        print(i, a_dictionary[i],sep=": ")
+        print(i, a_dictionary[i], sep=": ")
