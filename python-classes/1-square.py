47df4fe6c09942277c1c58651e3ff947a0f7780d
l-isaro <l.isaro@alustudent.com>
2023-11-13 05:24:28 -0800

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,10 +3,10 @@
 this module contains a class called square
 """
 
+
 class Square:
     """
     This class contains the private attribute size
     """
     def __init__(self, size):
         self.__size = size
-        
