040caea9efb4d697704dd2979d29637f10b9b08e
l-isaro <l.isaro@alustudent.com>
2023-11-13 04:48:47 -0800

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module contains the class square """
+
+
+class Square:
+    """
+    the following class defines attributes for an object square
+    """
+
+    __init__(self, size):
+    self.size = size
