f1c25378ba4f88a53062ef697d007a9397eb550a
l-isaro <l.isaro@alustudent.com>
2023-11-14 02:11:27 -0800

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module contains the class square
+"""
+
+class Square:
+    """ This class defines square objects
+    Attributes:
+    private size
+    """
+    def __init__(self, size=0):
+        self.__size = 0
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
