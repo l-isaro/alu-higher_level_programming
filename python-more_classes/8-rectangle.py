5b174064d2247738d143ca0ff530b3060d3031a7
l-isaro <l.isaro@alustudent.com>
2023-11-18 14:21:24 -0800

    8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -84,8 +84,8 @@ class Rectangle:
         elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         else:
-             if rect_1.area() > rect_2.area():
-                 return rect_1
-             elif rect_2.area() > rect_1.area():
-                 return rect_2
-             return rect_1
+            if rect_1.area() > rect_2.area():
+                return rect_1
+            elif rect_2.area() > rect_1.area():
+                return rect_2
+            return rect_1
