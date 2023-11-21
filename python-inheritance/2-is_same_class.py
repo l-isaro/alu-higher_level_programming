5f1187912e2bcdfa93c4ebfa9a1aaa5e73498808
l-isaro <l.isaro@alustudent.com>
2023-11-21 10:56:09 -0800

    Task done

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
+
+    """checks if object is an instance of class provided"""
+
     if isinstance(obj, a_class):
         return True
     else:
