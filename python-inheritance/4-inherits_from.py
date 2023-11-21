2feee48b904edc308231d63f2c88d112a7b0eaee
l-isaro <l.isaro@alustudent.com>
2023-11-21 11:34:49 -0800

    Task done

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this module contains the method inherits_from()"""
+
+
+def inherits_from(obj, a_class):
+    """ returns true if the object is an instance of a class 
+        that inherits from the specified class
+    """
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
