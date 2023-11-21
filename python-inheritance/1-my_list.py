fb733b5cdc08229bd53bad4b18533a50ef60af35
l-isaro <l.isaro@alustudent.com>
2023-11-21 07:32:38 -0800

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,10 +5,8 @@ This module contains the class my_list
 
 
 class Mylist(list):
-    """ a Brief description of the class
+    """ the class inherits from list and sorts the list"""
 
-    the class inherits from list and sorts the list
-    """
     def print_sorted(self):
         """ Returns a sorted list(ascending)"""
         print(sorted(self))
