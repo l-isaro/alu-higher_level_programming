6f73a4b6e04221584c63408108831f3fecc1d10b
l-isaro <l.isaro@alustudent.com>
2023-11-21 12:41:15 -0800

    Task done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module contains the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ This class contains the method area"""
+    def area(self):
+        raise Exception(area() is not implemented"")
