709bbcbf3e7e6d7818950d1f625ee08cd7947735
l-isaro <l.isaro@alustudent.com>
2023-11-21 11:38:43 -0800

    Task done

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     """ returns true if the object is an instance of a class 
         that inherits from the specified class
     """
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
