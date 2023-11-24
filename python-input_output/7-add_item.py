8174eda8d3f7485398f12a15817639d191ed3d9d
l-isaro <l.isaro@alustudent.com>
2023-11-24 08:42:11 -0800

    Task done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list,
-and then saves them to a file"""
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
+import sys
 
-from sys import argv as arg
-to_json = __import__("5-save_to_json_file").save_to_json_file
-from_json = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-with open(filename, "a+") as f:
-    try:
-        arr = from_json(filename)
-    except:
-        arr = []
-    for i in arg[1:]:
-        arr.append(i)
-    to_json(arr, filename)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
+try:
+    load = load_from_json_file("add_item.json")
+except ValueError:
+    load = []
+save_to_json_file(load + sys.argv[1:], "add_item.json")
