3bb8d4d0db4300924096f5b58a609851264e4495
l-isaro <l.isaro@alustudent.com>
2023-11-24 07:12:40 -0800

    Task done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """ This function appends to a file and tells the number
     of characters added"""
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "a+", encoding="utf-8") as f:
         f.write(text)
         f.read()
         return f.tell()
