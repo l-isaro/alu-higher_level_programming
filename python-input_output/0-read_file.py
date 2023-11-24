d1ab008839b75aeae94796dffac52550f310a4c8
l-isaro <l.isaro@alustudent.com>
2023-11-24 00:34:00 -0800

    Task done

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains a function that reads a file """
+
+def read_file(filename=""):
+    """ This function opens a file and reads it"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
