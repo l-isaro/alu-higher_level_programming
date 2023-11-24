6612912c4579853bb020da2e460d8ebad9b51dd3
l-isaro <l.isaro@alustudent.com>
2023-11-24 07:06:00 -0800

    Task done

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module conatins a function that writes to a text file"""
+
+
+def write_file(filename="", text=""):
+    """ This function appends to a file and tells the number
+    of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+        f.read()
+        return f.tell()
