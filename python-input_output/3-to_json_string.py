3ee36dea9ce695ead5375eae846fbb3cefe6ed3a
l-isaro <l.isaro@alustudent.com>
2023-11-24 08:10:51 -0800

    Task done

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!#!/usr/python3
+""" This module contains a function to_json_string"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """ This function returns the JSON representation
+    of an object """
+    return json.dumps(my_obj)
