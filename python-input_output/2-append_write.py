8cabaaac3435459ed65058e7d9ed74c5e5123541
l-isaro <l.isaro@alustudent.com>
2023-11-24 08:02:28 -0800

    Task done

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3                                                                                                                                   
+#!/usr/bin/python3
 """ This module conatins a function that writes to a text file"""
 
 
 def append_write(filename="", text=""):
-    """ This function appends to a file and tells the number 
+    """ This function appends to a file and tells the number
     of characters added"""
     with open(filename, "a+", encoding="utf-8") as f:
         f.write(text)
