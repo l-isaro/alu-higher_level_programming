0a18ad5d234d4ec2ba432d0ecc7664361110cc43
l-isaro <l.isaro@alustudent.com>
2023-11-29 02:38:52 -0800

    Task done

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates a database and table with constraints
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(
+id INT UNIQUE AUTO-INCREMENT PRIMARY KEY
+state_id INT NOT NULL and FOREIGN KEY REFERENCES states(id)
+name VARCHAR(256) can’t be null);
