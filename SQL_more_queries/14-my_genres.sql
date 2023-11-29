c001b2ea12e2256f6fa97dad2de3d52bbb9a3d1a
l-isaro <l.isaro@alustudent.com>
2023-11-29 06:50:39 -0800

    Task done

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-INNER JOIN tv_Show_genres ON tv_show.genres.id = tv_genres.id
+INNER JOIN tv_Show_genres ON tv_show_genres.id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_Shows.title = 'Dexter'
 ORDER BY tv_genres.name
