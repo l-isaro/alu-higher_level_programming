1a6cba2dbb06f10f3af284cbe8f1368ba0d2c4d1
l-isaro <l.isaro@alustudent.com>
2023-11-29 00:34:09 -0800

    Task done

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
--- creates a user
+-- creates a user and grants all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL
+GRANT ALL PRIVILEGES
 ON *.*
 TO 'user_0d_1'@'localhost';
