fdd6b13489e9a5a022fb78f52c5cde92ff06de34
l-isaro <l.isaro@alustudent.com>
2023-12-01 17:07:20 +0000

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Prints status of site"""
+""" Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
