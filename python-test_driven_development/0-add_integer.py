cd6c084d794ec9cabe29d5f5de8a700f5cd90f15
l-isaro <l.isaro@alustudent.com>
2023-12-11 02:57:08 -0800

    0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module contains the function add """
+
+def add_integer(a, b=98):
+    """ Given two integers return the  sum
+
+        :param a: int
+        :param b: int
+        :return: int
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer ")
+        if a isinstance(float):
+            a = int(a)
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+        if b isinstance(float):
+            b = int(b)
+
+    return a + b
