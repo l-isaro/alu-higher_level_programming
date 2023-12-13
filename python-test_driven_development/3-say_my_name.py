65e67bbbbd0f54b46d7754ea16275b4e867dd39d
l-isaro <l.isaro@alustudent.com>
2023-12-13 11:25:32 -0800

    3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" contains a function that prints names of a user"""
+
+def say_my_name(first_name, last_name=""):
+    """ prints the first and last name of a user"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
