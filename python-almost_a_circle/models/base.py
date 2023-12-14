16dd3f45ac6bc0f2d807ae4b80b96f1d98688087
l-isaro <l.isaro@alustudent.com>
2023-12-14 04:48:19 -0800

    1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,9 +7,10 @@ class Base:
         parameters:
         id(int)"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            self.__nb_objects += 1
-            self.id = self.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
