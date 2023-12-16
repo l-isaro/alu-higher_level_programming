7f3ae5faebf53857a6e653393c4fee32a310f506
l-isaro <l.isaro@alustudent.com>
2023-12-16 08:49:36 +0200

    done

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
         :param id: uniquely represents the square
     """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
