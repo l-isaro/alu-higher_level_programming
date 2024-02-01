7cd857c829d69712947a12425229fec2b1e72b86
l-isaro <l.isaro@alustudent.com>
2024-02-01 10:51:19 +0200

    done

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args.length > 1){
-    console.log('Arguments found');
-} else if (args.length == 0) {
-    console.log("No argument");
+if (args.length > 1) {
+  console.log('Arguments found');
+} else if (args.length === 0) {
+    console.log('No argument');
 } else {
-    console.log("Argument found");
+  console.log('Argument found');
 }
