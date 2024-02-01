569cb2b50ba02718d813e1a4b2df3f6766dbd535
l-isaro <l.isaro@alustudent.com>
2024-02-01 19:34:14 +0200

    done

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < sentence.length; index++) {
+  console.log(array[index]);   
+}
