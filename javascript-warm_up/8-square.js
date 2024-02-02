c32726e204c062cc97e92460ca8156a2998c7d71
l-isaro <l.isaro@alustudent.com>
2024-02-02 11:06:16 +0200

    done

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ const args = process.argv.slice(2);
 if (parseInt(args[0])) {
   for (let index = 0; index < parseInt(args[0]); index++) {
     for (let index = 0; index < parseInt(args[0]); index++) {
-      console.log("x");
+      console.log('x');
     }
-    console.log("\n");
+    console.log('\n');
   }
 }
