54d7a254dfb9dc0ec523212f73a3d8b476a58970
l-isaro <l.isaro@alustudent.com>
2024-02-02 12:45:50 +0200

    done

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,4 +6,6 @@ if (parseInt(args[0])) {
   for (let index = 0; index < parseInt(args[0]); index++) {
     console.log('X'.repeat(parseInt(args[0])));
   }
+} else {
+  console.log('Missing size');
 }
