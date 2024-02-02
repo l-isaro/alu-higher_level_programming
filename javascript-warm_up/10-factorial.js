7e41756b02bf93a6367741ce0283c2d84bf21b1c
l-isaro <l.isaro@alustudent.com>
2024-02-02 14:01:55 +0200

    done

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fact = parseInt(process.argv.slice(2)[0]);
+
+if (isNaN(fact) === true) {
+  console.log(1);
+} else {
+  const result = Factorial(fact);
+  console.log(result);
+}
+
+//prettier-ignore
+function Factorial (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return (num * Factorial(num - 1));
+  }
+}
