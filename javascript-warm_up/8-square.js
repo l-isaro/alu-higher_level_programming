861b5c1fabaa53d3b096dc4450c0492f5e8cf5e4
l-isaro <l.isaro@alustudent.com>
2024-02-02 12:40:45 +0200

    done

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,9 +4,6 @@ const args = process.argv.slice(2);
 
 if (parseInt(args[0])) {
   for (let index = 0; index < parseInt(args[0]); index++) {
-    for (let index = 0; index < parseInt(args[0]); index++) {
-      console.log('x');
-    }
-    console.log('\n');
+    console.log('x'.repeat(parseInt(args[0])));
   }
 }
