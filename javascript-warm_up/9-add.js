786126502cc86c4cce99c04077ce82f2d22d58a7
l-isaro <l.isaro@alustudent.com>
2024-02-02 13:53:10 +0200

    done

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-//prettier-ignore
-function add (a, b) { 
+// prettier-ignore
+function add (a, b) {
   const result = parseInt(a) + parseInt(b);
   console.log(result);
 }
