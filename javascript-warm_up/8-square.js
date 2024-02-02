f4e014bf150d596efe5879c20d4095338777e290
l-isaro <l.isaro@alustudent.com>
2024-02-02 10:51:29 +0200

    done

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (parseInt(args[0])) {
+  for (let index = 0; index < parseInt(args[0]); index++) {
+    for (let index = 0; index < parseInt(args[0]); index++) {
+        console.log('x');
+    }
+    console.log("\n");
+  }
+}
