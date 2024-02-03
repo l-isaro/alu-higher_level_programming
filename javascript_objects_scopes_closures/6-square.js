aabeffd83276594aa8333660abc364951690a2e1
l-isaro <l.isaro@alustudent.com>
2024-02-03 16:01:42 +0200

    done

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -8,13 +8,13 @@ module.exports = class Square extends OriginalSquare {
   }
   charprint (c) {
     if (c) {
-      for (let index = 0; index < this.size; index++) {
-        console.log('C'.repeat(this.size));
+      for (let index = 0; index < this.height; index++) {
+        console.log('C'.repeat(this.width));
         
       }
     } else {
-      for (let index = 0; index < this.size; index++) {
-        console.log('X'.repeat(this.size));
+      for (let index = 0; index < this.height; index++) {
+        console.log('X'.repeat(this.width));
         
       }
     }
