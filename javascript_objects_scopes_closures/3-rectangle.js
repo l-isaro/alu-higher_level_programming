73f1f38274ac34cee3d6a9db4bfa1d6c04de7564
l-isaro <l.isaro@alustudent.com>
2024-02-03 13:16:50 +0200

    done

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+
+    print () {
+      for (let index = 0; index < this.height; index++) {
+        console.log('x'.repeat(this.width));
+            
+      }
+    }
+  };
