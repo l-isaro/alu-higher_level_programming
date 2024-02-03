7bf8a579a01ff7c8354e473c08e17308c5bdfc77
l-isaro <l.isaro@alustudent.com>
2024-02-03 16:04:56 +0200

    done

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,19 +3,14 @@
 const OriginalSquare = require('./5-square');
 
 module.exports = class Square extends OriginalSquare {
-  constructor() {
-    super(size);
-  }
   charprint (c) {
     if (c) {
       for (let index = 0; index < this.height; index++) {
-        console.log('C'.repeat(this.width));
-        
+        console.log('C'.repeat(this.width));   
       }
     } else {
       for (let index = 0; index < this.height; index++) {
         console.log('X'.repeat(this.width));
-        
       }
     }
   }
