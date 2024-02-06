9fa49110b4b40b5446a57a83c041366ec912c59d
l-isaro <l.isaro@alustudent.com>
2024-02-06 21:35:05 +0200

    done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const fs = require('fs');
 
 fs.readFile(args[0], 'utf-8', (err, data) => {
-  if (error) {
+  if (err) {
     console.error(err);
   } else {
     console.log(data);
