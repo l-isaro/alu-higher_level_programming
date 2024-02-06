657d12711bef963e8a4a9acf64065ac4208443c6
l-isaro <l.isaro@alustudent.com>
2024-02-06 21:11:50 +0200

    correction of task1

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const args = process.argv.slice(2);
 const { error } = require('node:console');
 const fs = require('node:fs');
 
-const file_content = fs.readFileSync(args[0], 'utf-8', (error, data) => {
-  if(error) {
+fs.readFile(args, 'utf-8', (error, data) => {
+  if (error) {
     console.log(error);
   } else {
     console.log(data);
   }
-});
+})
