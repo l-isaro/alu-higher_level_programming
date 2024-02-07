9114c7ff961e32f2456928b655656ab29932e0a1
l-isaro <l.isaro@alustudent.com>
2024-02-07 11:29:03 +0200

    done

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const args = process.argv.slice(2);
+
+request.get(args[0], (err, res) => {
+  if (!err) {
+    console.log(`code: ${res.statusCode}`);
+  } else {
+    console.error(err);
+  }
+})
